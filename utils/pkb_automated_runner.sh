9ea1a33a94de0956af2fddc566b0b790f595138f
Besan A. Radwan <besan.aburadwan@rackspace.com>
2016-09-12 14:59:27 -0500

    Made style changes (@meteorfox)

--- utils/pkb_automated_runner.sh
+++ utils/pkb_automated_runner.sh
@@ -1,27 +1,25 @@
-VMS=$1
-OUT_DIRECTORY=$2
-CONFIG_FILE=$3
+#!/bin/bash -e
 
-if [ $# -lt 3 ]
-	then
-		echo "Too few arguments supplied, should be in format [vms, output directory, config file]"
-else
-	python fio_job_file_generator.py $CONFIG_FILE
-	for file in generated_fio_files/*
-		do
-		for vm in $(seq 1 $VMS)
-			do
-			UNIQUE_RUN_ID=`cat /dev/urandom | env LC_CTYPE=C tr -cd 'a-f0-9' | head -c 8`
-			./pkb.py --benchmarks=fio \
-			--run_uri=$UNIQUE_RUN_ID \
-			--num_vms=$vm \
-			--benchmark_config_file=pkb_fio_flags.yaml \
-			--fio_jobfile="$file"
-			if [ ! -d "$HOME/$OUT_DIRECTORY" ]; then
-				mkdir $HOME/$OUT_DIRECTORY
-			else
-				cp -r $HOME/tmp/perfkitbenchmarker/runs/$UNIQUE_RUN_ID $HOME/$OUT_DIRECTORY
-			fi
-		done
-	done
+VMS="$1"
+OUT_DIRECTORY="$2"
+CONFIG_FILE="$3"
+
+if [ $# -lt 3 ]; then
+  echo "Too few arguments supplied, should be in format [vms, output directory, config file]"
+  exit -1
 fi
+
+if [[ ! -d "$OUT_DIRECTORY" ]]; then
+    mkdir -p "$OUT_DIRECTORY"
+fi
+
+python fio_job_file_generator.py "$CONFIG_FILE"
+for file in generated_fio_files/*; do
+  for vm in $(seq 1 "$VMS"); do
+    UNIQUE_RUN_ID="$(cat /dev/urandom | env LC_CTYPE=C tr -cd 'a-f0-9' | head -c 8)"
+    python pkb.py --benchmarks=fio --run_uri="$UNIQUE_RUN_ID" \
+      --num_vms="$vm" --benchmark_config_file=pkb_fio_flags.yaml \
+      --fio_jobfile="$file"
+    cp -r /tmp/perfkitbenchmarker/runs/"$UNIQUE_RUN_ID" "$OUT_DIRECTORY"
+  done
+done
