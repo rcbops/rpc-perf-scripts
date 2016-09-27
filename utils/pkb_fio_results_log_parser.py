b540f2fb51a180874432c3fbc98b54fbdce6058b
Besan A. Radwan <besan.aburadwan@rackspace.com>
2016-09-27 15:40:58 -0500

    Changed input format to take in PKB Json log
    PKB by default has a JSON log that is generated after tests.
    Previously, we were parsing the pkb.log which is harder to parse
    and may be subject to change, whereas the JSON log is an expected format
    that an easily be parsed every time

--- utils/pkb_fio_results_log_parser.py
+++ utils/pkb_fio_results_log_parser.py
@@ -1,60 +1,89 @@
 import csv
+import json
 import fnmatch
 import subprocess
 import os
 import pathlib
 
 
-def find_pkb_logs():
+def find_pkb_json_logs():
 
     # finds all the pkb log files in all subsequent directories
     # and returns these logs as a list
     list_of_files = []
     for dirpath, dirs, files in os.walk(os.getcwd()):
-        for filename in fnmatch.filter(files, '*.log'):
+        for filename in fnmatch.filter(files, '*.json'):
             list_of_files.append(dirpath + "/" + filename)
     return list_of_files
 
 
-def consolidated_log(log_file):
+def json_file_consolidator(log_file):
 
-    # summarizes individual log file by only extracting relevant section
-    # and returns said relevant section
-    consolidated_log = subprocess.Popen(
-                     ['sed',
-                      '/PerfKitBenchmarker Complete Results/,/p99.99/!d;//d',
-                      log_file],
-                     stdout=subprocess.PIPE).communicate()[0].decode('ascii')
-    return consolidated_log
-
-
-def find_param_value(variable, log_file):
-
-    # parses summarized log file in order to find specific value, e.g. 'io'
-    # and returns value of passed in variable/parameter
-    brief_log = consolidated_log(log_file)
-    brief_log_no_spaces = brief_log.split()
-    brief_log_no_commas = ([s.replace(',', '') for s in brief_log_no_spaces])
-    brief_log_no_colons = ([s.replace(':', '') for s in brief_log_no_commas])
-    brief_log_no_quotes = ([s.replace("'", '') for s in brief_log_no_colons])
-
-    position_of_variable = 1 + brief_log_no_quotes.index(variable)
-    variable_value = brief_log_no_quotes[position_of_variable]
-    return variable_value
+    # grabs first two lines of the json file,
+    # as these are the only lines that contain the data
+    # we care about. This function returns an array of the
+    # two json lines we care about
+    data = []
+    with open(log_file) as f:
+        lines_after_2 = f.readlines()[:2]
+        for line in lines_after_2:
+            data.append(json.loads(line))
+        return data
 
 
-def parse_pkb_log(log_file):
+def parse_labels(labels):
+
+    # takes in string (the value to "labels" key)and removes
+    # commas and separators. Returns dictionary of parsed input string
+    # this function was taken from https://goo.gl/rpSmtW
+    result = {}
+    for item in labels.strip('|').split('|,|'):
+        k, v = item.split(':', 1)
+        result[k] = v
+    return result
+
+
+def get_labels(consolidated_log_file):
+
+    # calls parse_labels function on each element of the
+    # array passed in and returns an array of the dictionary value
+    # pertaining to the key "labels" for each dictionary in
+    # the passed in array
+    labels = []
+    for dict in consolidated_log_file:
+        parsed_labels_info = parse_labels(dict['labels'])
+        labels.append(parsed_labels_info)
+    return labels
+
+
+def find_key(parsed_labels, key):
+
+    # goes through the passed in parsed_labels dictionaries,
+    # and for each dictionary in parsed_labels checks to see if
+    # the desired key is there, if it is, it grabs the value
+    # of the key and returns it
+    for label_value in parsed_labels:
+        try:
+            value = label_value[key]
+            return value
+        except:
+            pass
+
+
+def report_dict_assign_values(log_file):
 
     # parse individual pkb log file for values
     # and returns a dictionary with these values
     report_dict = {}
+    consolidated_log_file = json_file_consolidator(log_file)
+    parsed_labels = get_labels(consolidated_log_file)
 
-    report_dict['io'] = find_param_value('iodepth', log_file)
-    report_dict['rwmixwrite'] = find_param_value('rwmixwrite', log_file)
-    report_dict['mean_bandwidth'] = find_param_value('bw_mean', log_file)
-    report_dict['mean_latency'] = find_param_value('mean', log_file)
-    report_dict['median_latency'] = find_param_value('p50', log_file)
-    report_dict['99th_p_latency'] = find_param_value('p99', log_file)
+    report_dict['io'] = find_key(parsed_labels, 'iodepth')
+    report_dict['rwmixwrite'] = find_key(parsed_labels, 'rwmixwrite')
+    report_dict['mean_bandwidth'] = find_key(parsed_labels, 'bw_mean')
+    report_dict['mean_latency'] = find_key(parsed_labels, 'mean')
+    report_dict['median_latency'] = find_key(parsed_labels, 'p50')
+    report_dict['99th_p_latency'] = find_key(parsed_labels, 'p99')
     return report_dict
 
 
@@ -68,9 +97,9 @@ def write_to_csv():
                       'median_latency', '99th_p_latency']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
-        list_of_log_files = find_pkb_logs()
+        list_of_log_files = find_pkb_json_logs()
         for log_file in list_of_log_files:
-            report_dict = parse_pkb_log(log_file)
+            report_dict = report_dict_assign_values(log_file)
             writer.writerow(report_dict)
 
 if __name__ == '__main__':
